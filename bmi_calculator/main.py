a753cf629137d37b8c5dc668c9963fe12b8cb837
prathamlokare2007 <prathamlokare2007@gmail.com>
2026-07-02 22:10:34 +0530

    Create main.py

---
+++ bmi_calculator/main.py
@@ -0,0 +1,6 @@
+# BMI Calculator
+weight=float(input("Enter the  weight in KG:"))       # take input  wirght in kg
+height=float(input("Enter the height in meter:"))     # take ip  height in meter
+
+BMI=(weight)/(height**2)                              # formula to calc BMI
+print("your BMI is:" ,BMI)                            # printing BMI output
