bfd97843796ef85cf8dd6dcfb477e9b101ae67e9
prathamlokare2007 <prathamlokare2007@gmail.com>
2026-07-02 22:41:49 +0530

    Create main.py

---
+++ madlibs_story/main.py
@@ -0,0 +1,6 @@
+# Mad Libs Story Generato
+namw=input("enter your name:")
+place=input("enter the place name:")
+fav_player=input("enter your favorite player name:")
+
+print("My name is",name,"I met my all time favorite player",fav_player,"at",place,"and took his autograph")
